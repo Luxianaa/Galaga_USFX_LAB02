feaf9cbc8cda1825b4fa047955355b41555651b0
Luxianaa <160753399+Luxianaa@users.noreply.github.com>
2024-03-17 19:32:15 -0400

    Se creo una malla para ESCaza

--- Source/Galaga_USFX_LAB02/NaveEnemiga.cpp
+++ Source/Galaga_USFX_LAB02/NaveEnemiga.cpp
@@ -8,7 +8,9 @@ ANaveEnemiga::ANaveEnemiga()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	mallaNaveEnemiga = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ShipMesh"));
+	mallaNaveEnemiga->SetupAttachment(RootComponent);
+	RootComponent = mallaNaveEnemiga;
 }
 
 // Called when the game starts or when spawned
