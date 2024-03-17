2ee09a6aafacf93184bfdd6cd5c12c5c66f5b4be
Luxianaa <160753399+Luxianaa@users.noreply.github.com>
2024-03-17 18:26:18 -0400

    Agregar archivos de proyecto.

---
+++ Source/Galaga_USFX_LAB02/Galaga_USFX_LAB02GameMode.cpp
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Galaga_USFX_LAB02GameMode.h"
+#include "Galaga_USFX_LAB02Pawn.h"
+
+AGalaga_USFX_LAB02GameMode::AGalaga_USFX_LAB02GameMode()
+{
+	// set default pawn class to our character class
+	DefaultPawnClass = AGalaga_USFX_LAB02Pawn::StaticClass();
+}
+
